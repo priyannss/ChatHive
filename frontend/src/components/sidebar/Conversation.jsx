437bfcc95990a66545034fd68cb5d4f6552437e7
Priyanshu Kumar <priyanshu@Priyanshus-MacBook-Air.local>
2024-09-28 20:13:05 +0530

    ui changes

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -13,7 +13,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
     return (
         <>
             <div className={`flex gap-2 items-center hover:bg-blue-500 rounded p-2 py-1 cursor-pointer
-                ${isSelected ? "bg-sky-500" : ""} `}
+                ${isSelected ? "bg-blue-500" : ""} `}
                 onClick={() => setSelectedConversation(conversation)}
             >
                 <div className={`avatar ${isOnline ? "online" : ""}`}>
